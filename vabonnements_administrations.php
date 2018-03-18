a95967b6140935c6e15f88711cbc9669810c5ed8
christophe le drean <christophe@rouge-rose.net>
2018-03-18 11:32:55 +0100

    vabonnements_administrations - supprimer les exemples du code - suppression des tables : on peut supprimer les colonnes ajoutées dans la table commandes. Pour les autres, laisser en commentaires le temps du développement pour éviter de supprimer les données par erreur.

--- vabonnements_administrations.php
+++ vabonnements_administrations.php
@@ -34,25 +34,7 @@ include_spip('base/vabonnements');
 **/
 function vabonnements_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
-	# quelques exemples
-	# (que vous pouvez supprimer !)
-	# 
-	# $maj['create'] = array(array('creer_base'));
-	#
-	# include_spip('inc/config')
-	# $maj['create'] = array(
-	#	array('maj_tables', array('spip_xx', 'spip_xx_liens')),
-	#	array('ecrire_config', 'vabonnements', array('exemple' => "Texte de l'exemple"))
-	#);
-	#
-	# $maj['1.1.0']  = array(array('sql_alter','TABLE spip_xx RENAME TO spip_yy'));
-	# $maj['1.2.0']  = array(array('sql_alter','TABLE spip_xx DROP COLUMN id_auteur'));
-	# $maj['1.3.0']  = array(
-	#	array('sql_alter','TABLE spip_xx CHANGE numero numero int(11) default 0 NOT NULL'),
-	#	array('sql_alter','TABLE spip_xx CHANGE texte petit_texte mediumtext NOT NULL default \'\''),
-	# );
-	# ...
-
+	
 	$maj['create'][] = array('maj_tables', array(
 		'spip_abonnements_offres',
 		'spip_abonnements',
@@ -88,16 +70,11 @@ function vabonnements_upgrade($nom_meta_base_version, $version_cible) {
  * @return void
 **/
 function vabonnements_vider_tables($nom_meta_base_version) {
-	# quelques exemples
-	# (que vous pouvez supprimer !)
-	# sql_drop_table('spip_xx');
-	# sql_drop_table('spip_xx_liens');
-
 	// champs du plugin
 	// sql_drop_table('spip_abonnements_offres');
 	// sql_drop_table('spip_abonnements');
-	// sql_alter('TABLE spip_commandes_details DROP numero_debut');
-	// sql_alter('TABLE spip_commandes_details DROP numero_fin');
+	sql_alter('TABLE spip_commandes_details DROP numero_debut');
+	sql_alter('TABLE spip_commandes_details DROP numero_fin');
 
 	// champs extra du plugin
 	cextras_api_vider_tables(vabonnements_declarer_champs_extras());
