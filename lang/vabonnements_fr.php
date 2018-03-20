0d3d762c635ace0896bff00ae17357df799d7f4f
christophe le drean <christophe@rouge-rose.net>
2018-03-20 10:24:54 +0100

    Correction chaîne de langue

--- lang/vabonnements_fr.php
+++ lang/vabonnements_fr.php
@@ -14,6 +14,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_taxe_defaut_explication' => 'TVA par défaut applicable aux offres d\'abonnement. Saisie de la forme 0.20 pour une TVA à 20% par exemple.',
 	'cfg_taxe_defaut_label' => 'TVA',
 	'cfg_titre_parametrages' => 'Paramétrages',
+	
+	// R
+	'reference_label' => 'Référence',
 
 	// T
 	'titre_config_reference_update' => 'Mise à jour des références des numéros de Vacarme',
