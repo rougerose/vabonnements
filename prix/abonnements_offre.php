3d4a8a9a0fb950b72f6ca9ebe79e8cb403c6b2b1
christophe le drean <christophe@rouge-rose.net>
2018-06-17 23:04:16 +0200

    Corrections

--- prix/abonnements_offre.php
+++ prix/abonnements_offre.php
@@ -3,12 +3,11 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function prix_abonnements_offre_dist($id_objet, $prix_ht){
-	$prix = $prix_ht;
 	
 	include_spip('inc/config');
 	$taxe = lire_config('vabonnements/taxe', 0.2);
-
-	$prix += $prix*$taxe;
+	
+	$prix = $prix_ht * (1 + $taxe);
 
 	return $prix;
 }
