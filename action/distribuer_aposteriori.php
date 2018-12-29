456b09dd5573f12d970f3a1c77dc4be747a75faf
christophe le drean <christophe@rouge-rose.net>
2018-12-30 00:01:58 +0100

    Vérifier les abonnements :
    - une page pour vérifier si chaque commande qui comporte un abonnement
    a bien créé l'abonnement.
    - dans l'hypothèse où il y a des abonnements manquants, ajout d'une
    action qui permet de relancer la distribution de la commande.

---
+++ action/distribuer_aposteriori.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) {
+	return;
+}
+
+
+function action_distribuer_aposteriori_dist($arg=null) {
+	if (is_null($arg)){
+		$securiser_action = charger_fonction('securiser_action', 'inc');
+		$arg = $securiser_action();
+	}
+	$arg = intval($arg);
+	$distribuer_commande = charger_fonction('distribuer_commande', 'action', true);
+	
+	$distribuer_commande($arg);
+}
